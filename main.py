94a2c750e8d5f08a988f4a078058a5bd34ef1b55
1180115 <1180115@isep.ipp.pt>
2022-06-07 23:25:42 +0100

    missing main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from app import views
+app = Flask(__name__)
+
+# url
+app.add_url_rule('/base','base',views.base)
+app.add_url_rule('/','index',views.index)
+app.add_url_rule('/stressapp','stressapp',views.stressapp)
+app.add_url_rule('/stressapp/stress','stress',views.stress,methods=['GET','POST'])
+app.add_url_rule('/stressapp/stress/<filename>/<status>','video',views.video,methods=['GET','POST'])
+
+# run
+if __name__ == "__main__":
+#    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
